18ca88e70cbb50d018125cd7f2d24c8ac1654292
Daniel Jowett <dbjowett@gmail.com>
2021-12-13 14:04:32 +0900

    Fix some more bugs

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -2,6 +2,8 @@ import ReactMarkdown from 'react-markdown';
 import PostHeader from './post-header';
 import classes from './post-content.module.css';
 import Image from 'next/image';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 export default function PostContent({ post }) {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
@@ -22,6 +24,12 @@ export default function PostContent({ post }) {
       } else {
         return <p>{paragraph.children}</p>;
       }
+    },
+    code(code) {
+      const { className, children } = code;
+      const language = className.split('-')[1]; // className is something like language-js => We need the "js" part here
+
+      return <SyntaxHighlighter style={dracula} language={language} children={children} />;
     }
   };
 
