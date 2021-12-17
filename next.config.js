729d2445339861e715a6abdf8bcf59d0ce66da70
Daniel Jowett <dbjowett@gmail.com>
2021-12-17 10:43:01 +0900

    Finalize a bunch of things

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-const { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD, PHASE_PRODUCTION_SERVER } = require('next/constants');
+// const { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD, PHASE_PRODUCTION_SERVER } = require('next/constants');
 
 module.exports = {
   // env: {
@@ -7,17 +7,17 @@ module.exports = {
   //   mongodb_clustername: 'cluster0',
   //   mongodb_database: 'my-blog'
   // },
-  reactStrictMode: true,
-  future: {
-    webpack5: true // by default, if you customize webpack config, they switch back to version 4.
-    // Looks like backward compatibility approach.
-  },
-  webpack(config) {
-    config.resolve.fallback = {
-      ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
-      // by next.js will be dropped. Doesn't make much sense, but how it is
-      fs: false // the solution
-    };
-    return config;
-  }
+  //   reactStrictMode: true,
+  //   future: {
+  //     webpack5: true // by default, if you customize webpack config, they switch back to version 4.
+  //     // Looks like backward compatibility approach.
+  //   },
+  //   webpack(config) {
+  //     config.resolve.fallback = {
+  //       ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
+  //       // by next.js will be dropped. Doesn't make much sense, but how it is
+  //       fs: false // the solution
+  //     };
+  //     return config;
+  //   }
 };
